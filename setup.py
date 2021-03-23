58fed1b6faab089ea3f575877a74a6fd7f901a80
Caspar Martens <caspar.martens@ost.ch>
2021-03-23 13:00:33 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="black_fennec",
-    version="0.1.2",
+    version="0.2.0",
     description="Black Fennec",
     long_description="Black Fennec",
     long_description_content_type="text/markdown",
