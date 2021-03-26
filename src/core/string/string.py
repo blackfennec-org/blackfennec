cdcdf576198d14a9c333ce1e4d100f6a603fb546
Simon Kindhauser <simon.kindhauser@ost.ch>
2021-03-26 14:02:31 +0100

    add docstrings to core string

--- src/core/string/string.py
+++ src/core/string/string.py
@@ -2,13 +2,22 @@ from collections import  UserString
 from src.core.info import Info
 
 class String(Info, UserString):
-    def __init__(self, value=""):
+    """Core Type String, represents strings in the domain model."""
+
+    def __init__(self, value: str = ""):
+        """Construct String with value `value`.
+
+        Args:
+            value (:obj:`str`, optional): The value of the `String`.
+                By default "" (empty string)
+        """
         super(Info, self).__init__(value)
 
     @property
     def value(self):
+        """"Property for the value of `String`"""
         return self.data
 
     @value.setter
-    def value(self, value):
+    def value(self, value:str):
         self.data = value
--- src/core/string/string_bidder.py
+++ src/core/string/string_bidder.py
@@ -1,5 +1,13 @@
+from src.core.info import Info
 from src.core.string import String
 
 class StringTypeBidder:
-    def bid(self, obj):
+    """The bidding service for the core type `String`."""
+
+    def bid(self, obj: Info):
+        """"Produces an offer for a given object.
+
+        Args:
+            obj (:obj:`Info`): The Info for which an offer should be produced.
+        """
         return isinstance(obj, String)
