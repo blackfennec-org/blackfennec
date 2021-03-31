92f9eccaab8cfb2f5bc5f1471ab126a28f26f90f
Caspar Martens <caspar.martens@ost.ch>
2021-03-31 16:15:39 +0200

    Add docstrings to auctioneer

--- src/core/auctioneer.py
+++ src/core/auctioneer.py
@@ -5,13 +5,52 @@ from src.core.offer import Offer
 
 
 class Auctioneer:
+    """Auctioneer Class.
+
+    Decides how Info is interpreted and creates
+    and Interpreter with the most suitable type
+
+    Attributes:
+        _type_registry (TypeRegistry): stores injected
+            type registry
+    """
     def __init__(self, type_registry):
+        """Auctioneer constructor.
+
+        Args:
+            type_registry(TypeRegistry): type registry to
+                retrieve registered types from.
+        """
         self._type_registry = type_registry
 
     def _select_offers(self, offers: [Offer]) -> [Offer]:
+        """Offer selection.
+
+        Gets a list of Offers and selects the most suitable
+        Can be multiple.
+
+        Args:
+            offers([Offer]): list of offers to choose from
+
+        Returns:
+            [Offer]: most suitable offers
+        """
         return [max(offers)]
 
-    def auction(self, subject: Info):
+    def auction(self, subject: Info) -> Interpreter:
+        """Auction of Info.
+
+        Auctions subject to all know types and each
+        type can make an offer of how capable he is
+        in handling the subject
+
+        Args:
+            subject(Info): Info to auction
+
+        Returns:
+            Interpreter: Interpreter capable of creating
+                an interpretation of the type
+        """
         types = dict()
         for bidder, factory in self._type_registry.types.items():
             types[bidder.bid(subject)] = factory
