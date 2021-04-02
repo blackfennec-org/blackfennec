221a6ddad35fc3a6c6238fb5c69b0768a69c9e5b
leonie <leonie.daeullaryost.ch>
2021-04-02 08:14:46 +0200

    implemented css provider

--- src/black_fennec_view.py
+++ src/black_fennec_view.py
@@ -1,6 +1,8 @@
 import logging
 import gi
 from gi.repository import Gtk
+from gi.repository import Gdk
+
 
 gi.require_version("Gtk", "3.0")
 
@@ -13,10 +15,16 @@ class BlackFennecView(Gtk.ApplicationWindow):
     _presenter_container = Gtk.Template.Child()
 
     def __init__(self, app, view_model):
+
         super().__init__(application=app)
         logger.info("BlackFennecView __init__")
         self._view_model = view_model
         self._presenter_container.add(self._view_model.presenter)
         self._presenter_container.show_all()
+
+        screen = Gdk.Screen.get_default()
+        provider = Gtk.CssProvider()
+        provider.load_from_path("src/style.css")
+        Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 
