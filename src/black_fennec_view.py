c2ab88b9a1abd2a7e12337baf5bb455c6faf20a6
leonie <leonie.daeullaryost.ch>
2021-04-02 10:01:40 +0200

    add click handler

--- src/black_fennec_view.py
+++ src/black_fennec_view.py
@@ -14,7 +14,7 @@ class BlackFennecView(Gtk.ApplicationWindow):
     __gtype_name__ = "BlackFennecView"
     _presenter_container = Gtk.Template.Child()
 
-    def __init__(self, app, view_model):
+    def __init__(self, app, view_model, click_handler=None):
 
         super().__init__(application=app)
         logger.info("BlackFennecView __init__")
@@ -26,5 +26,52 @@ class BlackFennecView(Gtk.ApplicationWindow):
         provider = Gtk.CssProvider()
         provider.load_from_path("src/style.css")
         Gtk.StyleContext.add_provider_for_screen(screen, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+        self._click_handler = click_handler
+
+    def _click_handler(self) -> None:
+        """Handles clicks on items"""
+
+    @Gtk.Template.Callback()
+    def on_new_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.new()
+        logger.debug('new clicked')
+
+    @Gtk.Template.Callback()
+    def on_open_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.open()
+        logger.debug('open clicked')
+
+    @Gtk.Template.Callback()
+    def on_quit_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.quit()
+        logger.debug('quit clicked')
+
+    @Gtk.Template.Callback()
+    def on_save_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.save()
+        logger.debug('save clicked')
+
+    @Gtk.Template.Callback()
+    def on_save_as_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.save_as()
+        logger.debug('save as clicked')
+
+    @Gtk.Template.Callback()
+    def on_go_to_store_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.go_to_store()
+        logger.debug('go to store clicked')
+
+    @Gtk.Template.Callback()
+    def on_about_and_help_clicked(self, _) -> None:
+        """Callback for the button click event"""
+        self._view_model.about_and_help()
+        logger.debug('about and help clicked')
 
 
--- src/black_fennec_view_model.py
+++ src/black_fennec_view_model.py
@@ -1,8 +1,34 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 class BlackFennecViewModel:
     def __init__(self, presenter):
+        logger.info("BlackFennecViewModel __init__")
         self._presenter = presenter
 
     @property
     def presenter(self):
         return self._presenter
+
+    def new(self):
+        logger.warning('new() not yet implemented')
+
+    def open(self):
+        logger.warning('open() not yet implemented')
+
+    def quit(self):
+        logger.warning('quit() not yet implemented')
+
+    def save(self):
+        logger.warning('save() not yet implemented')
+
+    def save_as(self):
+        logger.warning('save_as() not yet implemented')
+
+    def go_to_store(self):
+        logger.warning('go_to_store() not yet implemented')
+
+    def about_and_help(self):
+        logger.warning('about_and_help() not yet implemented')
