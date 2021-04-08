553a58a8fa78be75adf94573aea696588fdc308d
Caspar Martens <caspar.martens@ost.ch>
2021-04-08 17:17:32 +0200

    Add boolean tests

--- tests/core/boolean/test_boolean.py
+++ tests/core/boolean/test_boolean.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import unittest
 from doubles.core import RootMock
 from src.core.boolean import Boolean
+
 
 class BooleanTestSuite(unittest.TestCase):
     def test_can_construct(self):
@@ -44,3 +46,35 @@ class BooleanTestSuite(unittest.TestCase):
         actual = Boolean(True)
         expected = 'Boolean(%s)' % actual.value
         self.assertEqual(actual.__repr__(), expected)
+
+    def test_equal_equal_elements(self):
+        comp = Boolean(True)
+        equal_comp = Boolean(True)
+        self.assertTrue(
+            comp == equal_comp,
+            msg='Equal elements are not equal'
+        )
+
+    def test_equal_unequal_elements(self):
+        comp = Boolean(True)
+        other_comp = Boolean(False)
+        self.assertFalse(
+            comp == other_comp,
+            msg='Unequal elements are equal'
+        )
+
+    def test_not_equal_equal_elements(self):
+        comp = Boolean(True)
+        equal_comp = Boolean(True)
+        self.assertFalse(
+            comp != equal_comp,
+            msg='Equal elements are not equal'
+        )
+
+    def test_not_equal_unequal_elements(self):
+        comp = Boolean(True)
+        other_comp = Boolean(False)
+        self.assertTrue(
+            comp != other_comp,
+            msg='Unequal elements are equal'
+        )
