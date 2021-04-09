ea593e432d05ffce387417733fb6d5bb563e7200
Simon Kindhauser <simon.kindhauser@ost.ch>
2021-04-09 17:57:43 +0200

    switch to read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,8 +46,10 @@ release = '0.3.0'
 extensions = [
     'plantweb.directive',
     'sphinxcontrib.rsvgconverter',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -63,7 +65,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
+#html_logo = 'logo.svg'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
