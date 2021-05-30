6af65b3650da30e6bf24a2bc03291971f6e9242a
Simon Kindhauser <simon.kindhauser@ost.ch>
2021-05-30 09:31:48 +0200

    remove 'add' option from row context menu

--- src/visualisation/core/map/map_item_view.py
+++ src/visualisation/core/map/map_item_view.py
@@ -14,12 +14,7 @@ class MapItemView(Gtk.Bin):
     _preview_container: Gtk.Bin = Gtk.Template.Child()
     _popover = Gtk.Template.Child()
     _edit_popover = Gtk.Template.Child()
-    _add_popover = Gtk.Template.Child()
     _rename_entry = Gtk.Template.Child()
-    _add_entry = Gtk.Template.Child()
-    _template_store = Gtk.Template.Child()
-    _template_box = Gtk.Template.Child()
-
 
     def __init__(
             self,
@@ -78,11 +73,6 @@ class MapItemView(Gtk.Bin):
             self._edit_popover.popup()
         elif button == 'Delete':
             self._delete_request_handler(self)
-        elif button == 'Add':
-            self._popover.popdown()
-            self._add_popover.set_relative_to(self)
-            self._setup_template_store()
-            self._add_popover.popup()
         else:
             message = f'Unknown button({button}) clicked by {sender}'
             logger.warning(message)
@@ -93,37 +83,9 @@ class MapItemView(Gtk.Bin):
         self._edit_popover.popdown()
         self._view_model.rename_key(self.key, new_key)
 
-    @Gtk.Template.Callback()
-    def on_add_clicked(self, unused_sender):
-        key = self._add_entry.get_text()
-        self._add_popover.popdown()
-        template = self._get_template_by_string(
-            self._template_box.get_active_text())
-        self._view_model.add_by_template(key, template)
-
     def _delete_request_handler(self, sender):
         self._view_model.delete_item(sender.key)
 
     @Gtk.Template.Callback()
     def on_cancel_clicked(self, unused_sender):
         self._edit_popover.popdown()
-        self._add_popover.popdown()
-
-    def _setup_template_store(self):
-        template_store = Gtk.ListStore(GObject.TYPE_STRING)
-        templates = self._view_model.get_templates()
-        if templates:
-            for template in self._view_model.get_templates():
-                template_store.append((template.name,))
-        else:
-            template_store = self._template_store
-        self._template_box.set_model(template_store)
-
-    def _get_template_by_string(self, template_string: str):
-        for template in self._view_model.get_templates():
-            if template.name == template_string:
-                return template
-        message = f'Template({template_string}) could not be found ' \
-                  f'in template registry'
-        logger.error(message)
-        raise KeyError(message)
