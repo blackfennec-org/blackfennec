f48259b0806b48bc885574efe2586803cc14c41e
Caspar Martens <caspar.martens@ost.ch>
2022-10-17 08:48:42 +0200

    Redesign datetime base type

--- src/visualisation/base/date_time/date_time_preview.py
+++ src/visualisation/base/date_time/date_time_preview.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from gi.repository import Gtk, Adw
 
+from src.visualisation.base.date_time.date_time_editor import DateTimeEditor
 from src.visualisation.base.date_time.date_time_view_model import DateTimeViewModel
 
 logger = logging.getLogger(__name__)
@@ -12,11 +13,11 @@ UI_TEMPLATE = str(BASE_DIR.joinpath('date_time_preview.ui'))
 
 
 @Gtk.Template(filename=UI_TEMPLATE)
-class DateTimePreview(Adw.Bin):
+class DateTimePreview(Gtk.MenuButton):
     """View for the core type DateTime."""
 
     __gtype_name__ = 'DateTimePreview'
-    _date_time_value: Gtk.Label = Gtk.Template.Child()
+    _popover: Gtk.Popover = Gtk.Template.Child()
 
     def __init__(self, view_model: DateTimeViewModel):
         """Construct with view_model.
@@ -26,14 +27,13 @@ class DateTimePreview(Adw.Bin):
         """
         super().__init__()
         self._view_model = view_model
-        self._set_date_time()
+        self._view_model.bind(date_time=self._set_date_time)
+        self._set_date_time(self, self._view_model.date_time)
         logger.info('DateTimePreview created')
 
-    def _set_date_time(self):
-        date_time = self._view_model.date_time
-        self._date_time_value.set_text(str(date_time))
+        self.set_label(str(self._view_model.date_time))
+        self._date_time_editor = DateTimeEditor(self._view_model)
+        self._popover.set_child(self._date_time_editor)
 
-    @Gtk.Template.Callback()
-    def _preview_clicked(self, unused_sender, unused_argument) -> None:
-        """Handles clicks on date_time items, triggers navigation"""
-        self._view_model.navigate()
+    def _set_date_time(self, unused_sender, date_time):
+        self.set_label(str(date_time))
