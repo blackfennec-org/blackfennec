35dcbd22a77dd052e49b1a70ebc9106d2d11cfac
Simon Kindhauser <skindhauser@yodabyte.ch>
2022-11-09 18:19:28 +0100

    add tests for view factory

--- blackfennec_doubles/interpretation/double_interpretation.py
+++ blackfennec_doubles/interpretation/double_interpretation.py
@@ -6,15 +6,15 @@ class InterpretationMock:
     def __init__(self, 
             structure=None, 
             structure_view=None, 
-            specification=None):
+            specification=None,
+            types=None):
         self.navigation_requests = list()
         self.structure_property_access_count = 0
-        self._structure = StructureMock() if structure is None else structure
-        self.structure_view = (
-            StructureViewDummy() if structure_view is None else structure_view
-        )
+        self._structure = structure or StructureMock()
+        self.structure_view = structure_view or StructureViewDummy()
         self.navigation_service = None
         self.specification = specification or Specification()
+        self.types = types or []
 
     def set_navigation_service(self, navigation_service):
         self.navigation_service = navigation_service
