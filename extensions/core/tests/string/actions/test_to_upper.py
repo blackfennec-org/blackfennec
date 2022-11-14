c77dafa7084f79ca8777854da275bbd6678933c6
Caspar Martens <caspar.martens@ost.ch>
2022-11-14 09:46:14 +0100

    Leverage observability on number

--- extensions/core/tests/string/actions/test_to_upper.py
+++ extensions/core/tests/string/actions/test_to_upper.py
@@ -1,10 +1,12 @@
 from blackfennec_doubles.actions.double_context import ContextMock
-from core.string.actions import ToUpperAction
+from core.string.actions.to_upper import ToUpperAction
 from core import CORE_EXTENSION
+
 
 def test_can_construct():
     action = ToUpperAction()
     assert action is not None
+
 
 def test_can_execute():
     action = ToUpperAction()
@@ -13,13 +15,16 @@ def test_can_execute():
     action.execute(context)
     assert context.structure.value == "HELLO"
 
+
 def test_is_correct_type():
     action = ToUpperAction()
     assert action.type is CORE_EXTENSION.types.string
 
+
 def test_has_correct_name():
     action = ToUpperAction()
     assert action.name == "to upper"
+
 
 def test_has_correct_description():
     action = ToUpperAction()
