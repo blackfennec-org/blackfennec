d51815b704efe14826eb802e797c0896abf0b045
Caspar Martens <caspar.martens@ost.ch>
2022-11-17 23:22:46 +0100

    Add history tests

--- tests/blackfennec/layers/history/test_history.py
+++ tests/blackfennec/layers/history/test_history.py
@@ -3,52 +3,75 @@ from enum import Enum
 import random, string
 
 from blackfennec.layers.history.history import History
-from blackfennec_doubles.structure.double_structure import StructureMock, NotifyingStructureMock
-from blackfennec_doubles.layers.history.double_history_entry import HistoryEntryMock
+from blackfennec_doubles.structure.double_structure import StructureMock, \
+    NotifyingStructureMock
+from blackfennec_doubles.layers.history.double_history_entry import \
+    HistoryEntryMock
+
 
 @pytest.fixture
 def history():
     return History()
 
+
 @pytest.fixture
 def structure(history):
-    def observer(s, old, new): 
+    def observer(s, old, new):
         entry = HistoryEntryMock(structure=s, old=old, new=new)
         history.append(entry)
 
     return NotifyingStructureMock(observer, value='original')
 
+
 def test_can_construct(history):
     assert history
+
 
 def test_can_append(history, structure):
     structure.value = 'new'
     assert history.can_undo()
+
 
 def test_can_tell_if_can_redo(history, structure):
     structure.value = 'new'
     history.undo()
     assert history.can_redo()
 
-Action = Enum('Action', [ 'UNDO', 'REDO', 'CHANGE', 'RESULT' ])
+
+def test_can_redo(history):
+    assert not history.can_redo()
+
+
+def test_can_undo(history):
+    assert not history.can_undo()
+
+
+Action = Enum('Action', ['UNDO', 'REDO', 'CHANGE', 'RESULT'])
+
 
 def random_string():
     return ''.join(random.choice(string.hexdigits) for _ in range(16))
+
 
 @pytest.mark.parametrize('scenario', [
     ([Action.RESULT, Action.CHANGE, Action.UNDO]),
     ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO]),
     ([Action.RESULT, Action.CHANGE, Action.CHANGE, Action.UNDO, Action.UNDO]),
-    ([Action.CHANGE, Action.CHANGE, Action.RESULT, Action.UNDO, Action.UNDO, Action.REDO, Action.REDO]),
-    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO, Action.UNDO, Action.REDO]),
-    ([Action.CHANGE, Action.UNDO, Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO]),
+    ([Action.CHANGE, Action.CHANGE, Action.RESULT, Action.UNDO, Action.UNDO,
+      Action.REDO, Action.REDO]),
+    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO, Action.UNDO,
+      Action.REDO]),
+    ([Action.CHANGE, Action.UNDO, Action.CHANGE, Action.RESULT, Action.UNDO,
+      Action.REDO]),
 ])
 def test_can_undo_redo(structure, history, scenario):
     for action in scenario:
         match action:
             case Action.UNDO:
+                assert history.can_undo()
                 history.undo()
             case Action.REDO:
+                assert history.can_redo()
                 history.redo()
             case Action.RESULT:
                 result = structure.value
--- tests/blackfennec/layers/history/test_integration_history_layer.py
+++ tests/blackfennec/layers/history/test_integration_history_layer.py
@@ -1,3 +1,7 @@
+import random
+import string
+from enum import Enum
+
 import pytest
 
 from blackfennec.structure.map import Map
@@ -7,9 +11,11 @@ from blackfennec.structure.number import Number
 from blackfennec.structure.boolean import Boolean
 
 from blackfennec.layers.history.history import History
-from blackfennec.layers.history.history_factory_visitor import HistoryFactoryVisitor
+from blackfennec.layers.history.history_factory_visitor import \
+    HistoryFactoryVisitor
 
 pytestmark = pytest.mark.integration
+
 
 @pytest.fixture
 def structure():
@@ -21,37 +27,154 @@ def structure():
         "string": String("Leet")
     })
 
+
 @pytest.fixture
 def history():
     return History()
+
 
 @pytest.fixture
 def visitor(history):
     return HistoryFactoryVisitor(history)
 
+
 @pytest.fixture
-def historied(structure, visitor):
+def historized(structure, visitor):
     return structure.accept(visitor)
 
-def test_can_create_layer(historied):
-    assert historied
 
-def test_can_undo_modification_on_string(history, historied):
-    historied.value["string"].value = "Speak"
-    history.undo()
-    assert historied.value["string"].value == "Leet"
+def test_can_create_layer(historized):
+    assert historized
 
-def test_can_undo_modification_on_number_in_list(history, historied):
-    historied.value["list"].value[0].value = 3.141
-    history.undo()
-    assert historied.value["list"].value[0].value == 1337
 
-def test_can_undo_modification_on_list(history, historied):
-    historied.value["list"].add_item(Number(3.141))
+def test_can_undo_modification_on_string(history, historized):
+    historized.value["string"].value = "Speak"
     history.undo()
-    assert len(historied.value["list"].value) == 1
+    assert historized.value["string"].value == "Leet"
 
-def test_can_undo_modification_on_map(history, historied):
-    historied.remove_item("map")
+
+def test_can_undo_modification_on_number_in_list(history, historized):
+    historized.value["list"].value[0].value = 3.141
     history.undo()
-    assert historied.value["map"]
+    assert historized.value["list"].value[0].value == 1337
+
+
+def test_can_undo_add_on_list(history, historized):
+    historized.value["list"].add_item(Number(3.141))
+    history.undo()
+    assert len(historized.value["list"].value) == 1
+
+
+Action = Enum(
+    'Action',
+    ['UNDO', 'REDO', 'CHANGE', 'ADD_ITEM', 'REMOVE_ITEM', 'RESULT']
+)
+
+
+def random_string():
+    return ''.join(random.choice(string.hexdigits) for _ in range(16))
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('scenario', [
+    ([Action.RESULT, Action.CHANGE, Action.UNDO]),
+    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO]),
+    ([Action.RESULT, Action.CHANGE, Action.CHANGE, Action.UNDO, Action.UNDO]),
+    ([Action.CHANGE, Action.CHANGE, Action.RESULT, Action.UNDO, Action.UNDO,
+      Action.REDO, Action.REDO]),
+    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO, Action.UNDO,
+      Action.REDO]),
+    ([Action.CHANGE, Action.UNDO, Action.CHANGE, Action.RESULT, Action.UNDO,
+      Action.REDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.UNDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.ADD_ITEM, Action.UNDO,
+      Action.UNDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.ADD_ITEM, Action.UNDO,
+      Action.REMOVE_ITEM]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.REMOVE_ITEM, Action.UNDO,
+      Action.UNDO]),
+])
+def test_can_undo_redo_on_list(history, historized, scenario):
+    list_structure = historized.value["list"]
+    for action in scenario:
+        match action:
+            case Action.UNDO:
+                assert history.can_undo()
+                history.undo()
+            case Action.REDO:
+                assert history.can_redo()
+                history.redo()
+            case Action.RESULT:
+                result = list_structure.structure.value
+            case Action.CHANGE:
+                list_structure.value = [String(random_string())]
+            case Action.ADD_ITEM:
+                list_structure.add_item(String(random_string()))
+            case Action.REMOVE_ITEM:
+                list_structure.remove_item(
+                    random.choice(
+                        [
+                            value for value in list_structure.value
+                            if isinstance(value.structure, String)
+                        ]
+                    )
+                )
+    assert historized.value["list"].structure.value == result
+
+
+@pytest.mark.parametrize('scenario', [
+    ([Action.RESULT, Action.CHANGE, Action.UNDO]),
+    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO]),
+    ([Action.RESULT, Action.CHANGE, Action.CHANGE, Action.UNDO, Action.UNDO]),
+    ([Action.CHANGE, Action.CHANGE, Action.RESULT, Action.UNDO, Action.UNDO,
+      Action.REDO, Action.REDO]),
+    ([Action.CHANGE, Action.RESULT, Action.UNDO, Action.REDO, Action.UNDO,
+      Action.REDO]),
+    ([Action.CHANGE, Action.UNDO, Action.CHANGE, Action.RESULT, Action.UNDO,
+      Action.REDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.UNDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.ADD_ITEM, Action.UNDO,
+      Action.UNDO]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.ADD_ITEM, Action.UNDO,
+      Action.REMOVE_ITEM]),
+    ([Action.RESULT, Action.ADD_ITEM, Action.REMOVE_ITEM, Action.UNDO,
+      Action.UNDO]),
+])
+def test_can_undo_redo_on_map(history, historized, scenario):
+    map_structure = historized.value["map"]
+    for action in scenario:
+        match action:
+            case Action.UNDO:
+                assert history.can_undo()
+                history.undo()
+            case Action.REDO:
+                assert history.can_redo()
+                history.redo()
+            case Action.RESULT:
+                result = map_structure.structure.value
+            case Action.CHANGE:
+                map_structure.value = {
+                    random_string(): String(random_string())}
+            case Action.ADD_ITEM:
+                map_structure.add_item(random_string(),
+                                       String(random_string()))
+            case Action.REMOVE_ITEM:
+                map_structure.remove_item(
+                    random.choice(
+                        [key for key in map_structure.value.keys() if
+                         key != "boolean"]
+                    )
+                )
+
+    assert historized.value["map"].structure.value == result
+
+
+def test_can_undo_redo_on_top_level_map(history, historized):
+    result1 = historized.structure.value
+    historized.add_item("number", Number(1337))
+    result2 = historized.structure.value
+    historized.remove_item("number")
+    history.undo()
+    assert historized.structure.value == result2
+    history.undo()
+    assert historized.structure.value == result1
